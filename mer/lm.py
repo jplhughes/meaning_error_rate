d6d2f4ef146ad5a097d7c956cb9194c84865f2bb
John Hughes <jpl.hughes@btinternet.com>
2022-07-16 22:34:36 +0100

    add costing to lm class

--- mer/lm.py
+++ mer/lm.py
@@ -4,10 +4,19 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Cost for 1k tokens for each model
+models2cost = {
+    "text-davinci-002": 0.0600,
+    "text-curie-001": 0.0060,
+    "text-babbage-001": 0.0012,
+    "text-ada-001": 0.0008,
+}
+
 
 class LanguageModel:
     def __init__(self, model="text-davinci-002"):
         self.model = model
+        assert model in models2cost, f"Model {model} not supported"
 
     def get_continuation(
         self,
@@ -35,3 +44,8 @@ class LanguageModel:
         assert text is not None
 
         return text, response
+
+    def print_cost(self, prompt):
+        tokens = len(prompt) / 4  # each token is approximately 4 chars
+        cost = models2cost[self.model] * tokens / 1000
+        print(f"#char: {len(prompt)}, #token: {tokens}, cost: ${cost}, runs/$: {1/cost:.1f}")
