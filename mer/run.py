65721591280f95b3a150dd2566fcac3c5dd12bef
John Hughes <jpl.hughes@btinternet.com>
2022-07-16 22:53:39 +0100

    refactor run.sh to pass vars to function

--- mer/run.py
+++ mer/run.py
@@ -4,6 +4,31 @@ from mer.lm import LanguageModel
 from mer.prompt import Prompt
 
 
+def get_results_dbls(ref_dbl, rec_dbl, prompt_config_path):
+    ref_list = ref_dbl.read().split("\n")
+    rec_list = rec_dbl.read().split("\n")
+
+    assert len(ref_list) == len(rec_list), "Length of reference and recognised dbls differ"
+
+    prompt = Prompt.from_file(prompt_config_path)
+    lm = LanguageModel()
+
+    for ref_file, rec_file in zip(ref_list, rec_list):
+        with open(ref_file) as ref_h, open(rec_file) as rec_h:
+            ref = ref_h.read().strip()
+            rec = rec_h.read().strip()
+
+        prompt_string = prompt.create_prompt(ref, rec)
+        lm.print_cost(prompt_string)
+        break
+        text, _ = lm.get_continuation(prompt_string)
+        error_type, reason, score = prompt.get_result(text)
+
+        print(error_type)
+        print(reason)
+        print(score)
+
+
 def main():
 
     parser = argparse.ArgumentParser()
@@ -27,28 +52,7 @@ def main():
     )
     args = parser.parse_args()
 
-    ref_list = args.ref_dbl.read().split("\n")
-    rec_list = args.rec_dbl.read().split("\n")
-
-    assert len(ref_list) == len(rec_list), "Length of reference and recognised dbls differ"
-
-    prompt = Prompt.from_file(args.prompt_config_path)
-    lm = LanguageModel()
-
-    for ref_file, rec_file in zip(ref_list, rec_list):
-        with open(ref_file) as ref_h, open(rec_file) as rec_h:
-            ref = ref_h.read().strip()
-            rec = rec_h.read().strip()
-
-        prompt_string = prompt.create_prompt(ref, rec)
-        lm.print_cost(prompt_string)
-        break
-        text, _ = lm.get_continuation(prompt_string)
-        error_type, reason, score = prompt.get_result(text)
-
-        print(error_type)
-        print(reason)
-        print(score)
+    get_results_dbls(args.ref_dbl, args.rec_dbl, args.prompt_config_path)
 
 
 if __name__ == "__main__":
